0f6f1358c5614b452f07279fd1628c2ec1380456
Callum Flack <callumflack@gmail.com>
2017-10-05 23:11:01 +1000

    separate Header Root styling

--- Header.js
+++ Header.js
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 import PropTypes from 'prop-types'
-import {
-  site,
-  navItems,
-  contactDetails
-} from "../constants";
-import theme from '../theme'
-import icons from './iconConstants'
-import { headerFade } from './animationConstants'
 import styled, { css } from 'styled-components'
 import { Flex, Box } from 'rebass'
+
+import { site, navItems } from "../constants";
+import icons from './iconConstants'
+import { headerFade } from './animationConstants'
+import theme from '../theme'
+
+import HeaderRoot from './HeaderRoot';
 import Icon from './Icon';
 import { IconPhone } from './Icons'
 import { Display } from './Headings'
@@ -22,10 +21,6 @@ import Hamburger from './Hamburger'
 import MobileNav from './HeaderMobileNav'
 import DesktopNav from './HeaderDesktopNav'
 
-
-//
-// Text & buttons
-//
 
 const StyledButton = styled(ButtonOutline)`
   box-shadow: inset 0 0 0 1px;
@@ -50,114 +45,6 @@ const CTALink = props => (
   </Link>
 );
 
-//
-// Header root
-//
-
-const HeaderRoot = styled(({
-  isHidden,
-  isWindowScrolled,
-  isModalVisible,
-  children,
-  ...rest
-}) => (
-  <Flex is="header" {...rest}>{children}</Flex>
-))`
-  --Header-background-color: rbga(255, 255, 255, 1);
-  --Header-border-color: rgba(88, 88, 112, 0.15);
-  background-color: var(--Header-background-color);
-  border-bottom-width: 1px;
-  border-bottom-style: solid;
-  border-bottom-color: var(--Header-border-color);
-  height: ${theme.blockHeights.navBar};
-  opacity: 1;
-  position: fixed;
-  top: 0;
-  transform: translate3d(0, 0, 0) translateY(0px);
-  transition:
-    opacity ${headerFade.duration}s ease-in-out ${headerFade.delay}s,
-    transform ${headerFade.duration}s ease-in-out ${headerFade.delay}s,
-    background-color ${headerFade.duration}s ease-in-out,
-    border ${headerFade.duration}s ease-in-out 0s;
-  width: 100%;
-  z-index: 3;
-
-  @media (max-width: 512px) {
-    button {
-      padding: 0 0.75rem;
-    }
-  }
-
-  @media (max-width: 320px) {
-    button span {
-      display: none;
-    }
-  }
-
-  ${props => props.color && css`
-    color: ${theme.colors[props.color] || theme.colors.text};
-  `}
-
-  ${props => props.clear && css`
-    --Header-background-color: rbga(255, 255, 255, 0);
-  `}
-
-  ${props => props.reverseBorder && css`
-    --Header-border-color: rgba(255, 255, 255, 0.15);
-  `}
-
-  ${props => props.isHidden && css`
-    opacity: 0;
-    transform: translate3d(0, 0, 0) translateY(-${theme.blockHeights.navBar});
-    transition:
-      opacity ${headerFade.duration}s ease-in-out 1s,
-      transform ${headerFade.duration}s ease-in-out 1s,
-      background-color ${headerFade.duration}s ease-in-out,
-      border ${headerFade.duration}s ease-in-out 0s;
-  `}
-
-  ${props => props.isWindowScrolled && css`
-    --Header-border-color: transparent;
-    background-color: ${theme.colors[props.bg] || 'rgba(255, 255, 255, 1)'};
-    box-shadow:
-      0 16px 24px 2px rgba(0,0,0,0.03),
-      0 6px 30px 5px rgba(0,0,0,0.03);
-    color: ${theme.colors.text};
-    transition:
-      opacity ${headerFade.duration}s ease-in-out,
-      transform ${headerFade.duration}s ease-in-out,
-      background-color ${headerFade.duration}s ease-in-out,
-      border ${headerFade.duration}s ease-in-out;
-  `}
-
-  ${props => props.isModalVisible && css`
-    --Header-background-color: ${theme.colors.offWhite};
-    --Header-border-color: rgba(88, 88, 112, 0.15);
-    background-color: var(--Header-background-color);
-    border-bottom: 1px solid var(--Header-border-color);
-    transition:
-      opacity ${headerFade.duration}s ease-in-out,
-      transform ${headerFade.duration}s ease-in-out,
-      background-color ${headerFade.duration}s ease-in-out,
-      border ${headerFade.duration}s ease-in-out;
-  `}
-
-  ${props => props.invertTextOnMobile && css`
-    @media (max-width: 512px) {
-      --Header-border-color: rgba(255, 255, 255, 0.15);
-
-      a,
-      span {
-        color: ${theme.colors.text20} !important;
-      }
-
-      button {
-        border-color: --Header-border-color;
-      }
-    }
-  `}
-`;
-
 const Root = props => (
   <div>
     <HeaderRoot
@@ -165,7 +52,7 @@ const Root = props => (
       isWindowScrolled={props.isWindowScrolled}
       isModalVisible={props.isModalVisible}
     >
-      <Container px={[1, 2, 2, 3]} mw="lg" w={1}>
+      <Container px={[1, 2, 2, 3]} maxWidth="72em" w={1}>
 
         {/* MENU */}
         <RespToggle break={["none", "none", "block"]}>
@@ -175,8 +62,9 @@ const Root = props => (
           />
         </RespToggle>
 
+        {/* LOGO, CTA & HAMBURGER */}
         <Flex align="center" justify="space-between">
-          {/* LOGO */}
+          
           <Box>
             <Link href="/">
               <a>
@@ -189,7 +77,6 @@ const Root = props => (
             </Link>
           </Box>
 
-          {/* CTA */}
           <RespToggle break={["block", "block", "none", "block"]}>
             {site === "strata" && (
               <CTALink href={"tel:" + contactDetails.phone}>
@@ -203,7 +90,6 @@ const Root = props => (
             )}
           </RespToggle>
 
-          {/* HAMBURGER */}
           <RespToggle break={[null, null, null, "none"]} hideAtDesktop p={2}>
             <Hamburger
               onClick={props.handleModalTriggerClick}
@@ -226,7 +112,6 @@ const Root = props => (
     />
   </div>
 );
-
 
 
 class Header extends React.Component {
@@ -293,29 +178,6 @@ class Header extends React.Component {
   }
 
   render() {
-    const desktopNavItems = [
-      {
-        label: "Strata Management",
-        href: "/strata-management"
-      },
-      {
-        label: "For Landlords",
-        href: "/rental-property-management-for-landlords"
-      },
-      {
-        label: "For Tenants",
-        href: "/rental-property-management-for-tenants"
-      },
-      {
-        label: "Valuations",
-        href: "/useful-info"
-      },
-      {
-        label: "Contact",
-        href: "/contact"
-      }
-    ];
-    
     return (
       <Root
         handleModalClick={this.handleModalClick}
